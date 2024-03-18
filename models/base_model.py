b89d5556db0f04d79f0d78fe035632a140f7c032
ngari-kev <kevngariwangui@gmail.com>
2024-03-18 09:30:20 -0700

    BaseModel from Dict

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,24 @@ class BaseModel():
     This is a class that other classes inherit from.
 
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         initializes a new instance.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            if 'created_at' in kwargs:
+                tfom = '%Y-%m-%dT%H:%M:%S.%f'
+                self.created_at = datetime.strptime(kwargs['created_at'], tfom)
+            if 'updated_at' in kwargs:
+                tfom = '%Y-%m-%dT%H:%M:%S.%f'
+                self.updated_at = datetime.strptime(kwargs['updated_at'], tfom)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
